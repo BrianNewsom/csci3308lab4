af8cb9757d84af6c93a10d0e8ce3d9c19e9670c8
Brian Newsom <Brian.Newsom@Colorado.edu>
2014-09-17 15:38:25 -0600

    Update helloworld.java

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,5 +3,6 @@ package helloworld;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string.
+		System.out.println("Test pull from git into eclipse.");
 	}
 }
